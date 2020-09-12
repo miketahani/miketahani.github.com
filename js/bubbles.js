8665170964f6d0cec14b802d994ed660df570b4b
Mike Tahani <me@miketahani.com>
2020-09-12 13:51:19 -0700

    update

---
+++ js/bubbles.js
@@ -0,0 +1,149 @@
+(function () {
+
+  const twoPi = 2 * Math.PI;
+  const rand = Math.random;
+  const randFloor = max => rand() * max | 0;
+  const randSigned = max => rand() < 0.5 ? -rand() : rand();
+
+  // The bubble animation class
+  class AnimatedBubble {
+    constructor (xMax, yMax, stepSize) {
+      this.xMax = xMax
+      this.yMax = yMax
+      this.stepSize = stepSize
+      this.r = randFloor(5) + 2
+      this.x = randFloor(xMax)
+      this.y = randFloor(yMax)
+      this.angle = rand()
+    }
+
+    tick () {
+      const { xMax, yMax, x, y, r } = this
+
+      // move to next position
+      this.angle = this.angle + randSigned() * 0.1
+      const angleRad = this.angle * twoPi
+      this.x = x + this.stepSize * Math.cos(angleRad)
+      this.y = y + this.stepSize * Math.sin(angleRad)
+
+      // check boundaries
+      // the calculations using radius ensure that we don't see sudden
+      // disappearances (cx, cy of the circle are in its center, so we
+      // need to account for its remaining radius still on the canvas
+      // when cx, cy go outside the bounds)
+      if (this.x < -r) this.x = xMax + r;
+      if (this.x > xMax + r) this.x = -r;
+      if (this.y < -r) this.y = yMax + r;
+      if (this.y > yMax + r) this.y = -r;
+
+      return [this.x, this.y]
+    }
+  }
+
+  class AnimatedDyingBubble extends AnimatedBubble {
+    constructor (initialX, initialY, maxGenerations, xMax, yMax, stepSize) {
+      super(xMax, yMax, stepSize)
+
+      this.x = initialX
+      this.y = initialY
+
+      this.maxGenerations = maxGenerations
+      this.generation = 0
+      this.isDead = false
+
+      // let [seed1, seed2] = [rand(), rand()].sort((a, b) => a - b)
+      // this.seed1 = seed1
+      // this.seed2 = seed2
+      this.seed = rand()
+
+
+      this.maxSize = randFloor(15) + 3
+    }
+
+    tick () {
+      if (this.isDead) {
+        return
+      }
+      if (++this.generation > this.maxGenerations) {
+        this.isDead = true
+        return
+      }
+      super.tick()
+    }
+  }
+
+  // FIXME new CanvasConfig({  }), CanvasConfig extends Config
+  const config = {
+    fontSize: '500px',
+    fontFamily: 'Helvetica',
+    fontWeight: 'bold',
+    text: 'HELLO',
+    get font () {
+      return `${this.fontWeight} ${this.fontSize} ${this.fontFamily}`
+    }
+  }
+
+  const width = window.innerWidth
+  const height = window.innerHeight
+
+  const canvas = document.createElement('canvas')
+  canvas.width = width
+  canvas.height = height
+  document.getElementById('background').append(canvas)
+  const ctx = canvas.getContext('2d')
+
+  ctx.strokeStyle = 'rgba(0, 0, 0, 0.25)'
+
+  const maxGenerations = 70
+  let bubbles = []
+
+  function drawCoordinates ({ clientX: x, clientY: y }) {
+    for (let i = 0; i < 3; i++) {
+      const bubble = new AnimatedDyingBubble(x, y, maxGenerations, width, height, 2)
+      bubbles.push(bubble)
+    }
+  }
+
+  function drawBubble ({ x, y, r, generation, seed, maxSize }) {
+    const normGen = generation / maxGenerations
+    ctx.fillStyle = `hsl(${seed * 360}, 100%, ${100 - generation / maxGenerations * 40 + 20}%)`
+    ctx.beginPath()
+    ctx.arc(x, y, normGen * maxSize + 1, 0, 2 * Math.PI)
+    ctx.fill()
+    ctx.stroke()
+  }
+
+  let isRendering = false
+
+  window.addEventListener('pointermove', e => {
+    drawCoordinates(e)
+    // avoid duplicate renders
+    if (!isRendering) {
+      isRendering = true
+      requestAnimationFrame(render)
+    }
+  })
+
+  function render () {
+    if (isRendering) {
+      requestAnimationFrame(render)
+    }
+    let len = bubbles.length
+    if (!len) {
+      isRendering = false
+      return
+    }
+    let nextBubbles = []
+    for (let i = 0; i < len; i++) {
+      const bubble = bubbles[i]
+      bubble.tick()
+      if (bubble.isDead) {
+        continue
+      }
+      nextBubbles.push(bubble)
+      drawBubble(bubble)
+    }
+
+    bubbles = nextBubbles
+  }
+})();
---
+++ js/name-header.js
@@ -0,0 +1,93 @@
+(function () {
+  const LARGE_TEXT = 'MIKE'
+  const SUB_TEXT = 'TAHANI'
+  const COLORS = [
+    '#FF0033',
+    '#0099FF',
+    '#00FF00',
+    '#FF9900'
+  ]
+
+  const maxTravelDistance = 20
+
+  // use -20 for "MTXT" -- something about letter forms in "MIKE" look good offsetted
+  // const LARGE_TEXT_Y_OFFSET = -20 // for MXTX
+  const LARGE_TEXT_Y_OFFSET = 0 // for MIKE
+
+  const width = 900
+  const height = 400
+  const halfWidth = width / 2
+  const halfHeight = height / 2
+
+  const canvas = document.createElement('canvas')
+  canvas.width = width
+  canvas.height = height
+  document.getElementById('header').appendChild(canvas)
+  const ctx = canvas.getContext('2d')
+
+  ctx.textAlign = 'center'
+
+  ctx.font = '180px RoadRage'
+  const size = ctx.measureText(LARGE_TEXT)
+  const textHeight = size.actualBoundingBoxAscent - size.actualBoundingBoxDescent
+
+  ctx.font = 'bold 80px Helvetica'
+  const subSize = ctx.measureText(SUB_TEXT)
+  const subTextHeight = size.actualBoundingBoxAscent - size.actualBoundingBoxDescent
+
+  // Will be multiplied by x and y coordinates to determine a direction of movement
+  //  for each animated text block.
+  // FIXME use bitshifting or something
+  const textDirections = [
+    [-1, -1],
+    [ 1,  1],
+    [-1,  1],
+    [ 1, -1]
+  ]
+
+  const len = textDirections.length
+
+  function update (x, y) {
+    ctx.clearRect(0, 0, width, height)
+
+    ctx.globalAlpha = 1.0
+    ctx.globalCompositeOperation = 'source-over'
+
+    ctx.fillStyle = 'black'
+    ctx.fillRect(width / 4, halfHeight - textHeight * 1.5, halfWidth, textHeight * 1.5)
+
+    ctx.font = 'bold 80px Helvetica'
+    ctx.fillText(SUB_TEXT, halfWidth, halfHeight + subTextHeight / 1.5)
+
+    ctx.font = '180px RoadRage'
+    ctx.fillStyle = 'white'
+    ctx.fillText(LARGE_TEXT, halfWidth, halfHeight + LARGE_TEXT_Y_OFFSET)
+
+    ctx.globalAlpha = 0.5
+    ctx.globalCompositeOperation = 'screen'
+
+    const ax = Math.sin(x * 0.01) // Next x angle, cached
+    const ay = Math.cos(y * 0.01) // Next y angle, cached
+
+    for (let i = 0; i < len; i++) {
+      const direction = textDirections[i]
+      ctx.fillStyle = COLORS[i]
+      ctx.fillText(
+        LARGE_TEXT,
+        halfWidth + direction[0] * maxTravelDistance * ax,
+        halfHeight + LARGE_TEXT_Y_OFFSET + direction[1] * maxTravelDistance * ay
+      )
+    }
+  }
+
+  window.addEventListener('mousemove', e => {
+    update(
+      e.clientX * 0.5 - window.innerWidth,
+      e.clientY * 0.5 - window.innerHeight
+    )
+  })
+
+  document.fonts.ready.then(_ => {
+    update(halfWidth, halfHeight)
+  })
+})();
