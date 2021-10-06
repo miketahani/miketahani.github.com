d2b54fc12d2f2e7a1fccd3709f56a59ea7559747
Mike Tahani <me@miketahani.com>
2021-10-06 16:18:58 -0700

    add event listeners to links class

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # miketahani.github.io
-
--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ const initBubbles = require('./bubbles.js')
 const initNameHeader = require('./name-header.js')
 
 // Add listeners to change link background color
-Array.from(document.querySelectorAll('#links a')).forEach(el => {
+Array.from(document.querySelectorAll('.links a')).forEach(el => {
   el.addEventListener('mouseover', () => {
     el.style.background = `hsl(${Math.random() * 360 | 0}, 100%, 70%)`
   })
